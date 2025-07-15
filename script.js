5a9150b0828b40da1b5a694ef1927a8765078707
Tifany Trindade <99377193+TifanyTrindade@users.noreply.github.com>
2025-07-15 12:10:31 -0300

    atualizacao js

--- script.js
+++ script.js
@@ -2,31 +2,24 @@
 document.getElementById("bg-video").playbackRate = 0.6; // 0.5 = metade da velocidade
 
 //Ação de clique
-// Remove o amarelo ao voltar para a página
-window.addEventListener("pageshow", () => {
-  document.querySelectorAll(".button").forEach(btn => {
-    btn.classList.remove("clicked");
-  });
-});
-
-// Mantém o botão amarelo até o link carregar
-document.querySelectorAll(".button").forEach(btn => {
+document.querySelectorAll(".button").forEach((btn) => {
   btn.addEventListener("click", function (e) {
+    e.preventDefault(); // evita que o link abra imediatamente
+
     btn.classList.add("clicked");
 
-    const link = btn.getAttribute("href");
-    const target = btn.getAttribute("target");
-
     setTimeout(() => {
+      btn.classList.remove("clicked");
+
+      // Agora sim abre o link
+      const link = btn.getAttribute("href");
+      const target = btn.getAttribute("target");
+
       if (target === "_blank") {
         window.open(link, "_blank");
       } else {
         window.location.href = link;
       }
-    }, 300);
-
-    e.preventDefault();
+    }, 150); // tempo do efeito antes do link abrir
   });
 });
-
-
