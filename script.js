96786511b39149d445de794feb9758086520140d
Tifany Trindade <99377193+TifanyTrindade@users.noreply.github.com>
2025-07-15 11:50:03 -0300

    teste

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 // Quando o vídeo carregar, diminui a velocidade
 document.getElementById("bg-video").playbackRate = 0.6; // 0.5 = metade da velocidade
+
+//Ação de clique
+// Remove o amarelo ao voltar para a página
+window.addEventListener("pageshow", () => {
+  document.querySelectorAll(".button").forEach(btn => {
+    btn.classList.remove("clicked");
+  });
+});
+
+// Mantém o botão amarelo até o link carregar
+document.querySelectorAll(".button").forEach(btn => {
+  btn.addEventListener("click", function (e) {
+    btn.classList.add("clicked");
+
+    const link = btn.getAttribute("href");
+    const target = btn.getAttribute("target");
+
+    setTimeout(() => {
+      if (target === "_blank") {
+        window.open(link, "_blank");
+      } else {
+        window.location.href = link;
+      }
+    }, 300);
+
+    e.preventDefault();
+  });
+});
+
+
