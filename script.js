0e8880c22f95429d24b9e69a0f0d9f912827fe84
Tifany Trindade <99377193+TifanyTrindade@users.noreply.github.com>
2025-07-15 12:16:25 -0300

    Atualizacao teste celular

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Quando o vídeo carregar, diminui a velocidade
-document.getElementById("bg-video").playbackRate = 0.6; // 0.5 = metade da velocidade
+document.getElementById("bg-video").playbackRate = 0.7; // 0.5 = metade da velocidade
 
 //Ação de clique
 document.querySelectorAll(".button").forEach((btn) => {
